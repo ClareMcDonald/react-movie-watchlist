051895cdb390dc19855830df3b886a6468a62e96
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-17 14:38:46 -0800

    wrote signUp, signIn, and getUser in fetch-utils

---
+++ src/AuthPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function AuthPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignIn(e) {
+  
+  }
+  
+  return (
+    <div>
+      <form>
+
+      </form>
+    </div>
+  );
+}
