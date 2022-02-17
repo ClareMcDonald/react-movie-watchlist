6b7ca077e6150791767bb8870d4b9a7c94a5d940
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-17 14:57:01 -0800

    worked on AuthPage

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { useState } from 'react';
+import { signIn, signUp } from './services/fetch-utils.js';
 
-export default function AuthPage() {
+export default function AuthPage({ setUser }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   async function handleSignIn(e) {
-  
+    e.preventDefault();
+
+    const user = await signIn(email, password);
+
+    setUser(user);
   }
   
+  async function handleSignUp() {
+    const user = await signUp(email, password);
+
+    setUser(user);
+  }
+
   return (
     <div>
-      <form>
-
+      <form onSubmit={handleSignIn}>
+        <label> Email
+          <input onChange={e => setEmail(e.target.value)} value={email}/>
+        </label>
+        <label> Password
+          <input onChange={e => setPassword(e.target.value)} value={password} />
+        </label>
+        <button>SignIn</button>
       </form>
+      <button onClick={handleSignUp}>SignUp</button>
     </div>
   );
 }
