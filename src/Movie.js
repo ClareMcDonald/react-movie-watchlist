f2209ea8e62e48d32fc2cc6ae9ea29371af16dba
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-18 14:41:42 -0800

    rendered movies in movie.js and added a css image size; searched movies display

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import MovieList from './MovieList';
 
-export default function Movie() {
+export default function Movie({ movie }) {
   return (
-    <div>Movie</div>
+    <div>
+      <h3>{movie.title}</h3>
+      <h5>{movie.overview}</h5>
+      <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
+    </div>
   );
 }
