daf0776b8e4efaeb152ca55834c0785b9e9a30a5
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-17 17:55:13 -0800

    wrote WatchlistPage.js

--- src/WatchlistPage.js
+++ src/WatchlistPage.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { getWatchlistItems } from './services/fetch-utils';
+import WatchlistItem from './WatchlistItem';
 
 export default function WatchlistPage() {
+  const [movies, setMovies] = useState([]);
+   
+  async function fetchWatchlist() {
+    const watchList = await getWatchlistItems();
+    setMovies(watchList);
+      
+  }
+    
+  useEffect(() => {
+      
+    fetchWatchlist();
+        
+  }, []);
+
   return (
-    <div>WatchlistPage</div>
+    <div>
+      {movies.map((movie, i) => <WatchlistItem key={`${movie.title}-${i}`} movie={movie}/>)}
+    </div>
   );
 }
