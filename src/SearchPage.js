ebd9309e2585af947e91f9a73191681e058f1637
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-18 10:17:00 -0800

    set state and added handleSearch function to SearchPage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 export default function SearchPage() {
+  const [searchTitle, setSearchTitle] = useState();
+  const [movies, setMovies] = useState([]);
+  
+    handleSearch(e) {
+        e.preventDefault();
+
+        const movieResults = await searchMovies(searchTitle);
+
+        setMovies(movieResults);
+    }
+    
   return (
     <div>SearchPage</div>
   );
