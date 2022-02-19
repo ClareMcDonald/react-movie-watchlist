cc75081da1b7064d74ab886a6395cc0266ea923b
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-18 16:44:08 -0800

    added emoji for haveWatched on Movie.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { searchMovies } from './services/fetch-utils.js';
+import { searchMovies, getWatchlistItems } from './services/fetch-utils.js';
 import MovieList from './MovieList.js';
 
 export default function SearchPage() {
   const [searchTitle, setSearchTitle] = useState('');
-    const [movies, setMovies] = useState([]);
-    const [watchlist, setWatch]
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
   
   async function handleSearch(e) {
     e.preventDefault();
@@ -16,11 +16,17 @@ export default function SearchPage() {
     setMovies(movieResults);
   }
    
-    function isOnWatchlist(api_id) {
-        const match = watchlist.find(item => Number(item.api_id === Number(api_id));
+  async function fetchWatchlist() {
+    const watchList = await getWatchlistItems();
+    setWatchlist(watchList);
+      
+  }
+    
+  function isOnWatchlist(api_id) {
+    const match = watchlist.find(item => Number(item.api_id === Number(api_id)));
 
-        return Boolean(match)
-    }
+    return Boolean(match);
+  }
 
   return (
     <div>
@@ -30,7 +36,7 @@ export default function SearchPage() {
       </form>
       <section>
         Movie results:
-        <MovieList movies={movies} isOnWatchlist={isOnWatchlist} />
+        <MovieList movies={movies} isOnWatchlist={isOnWatchlist} fetchWatchlist={fetchWatchlist} />
       </section>
     </div>
       
